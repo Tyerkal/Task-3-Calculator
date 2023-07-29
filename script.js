7a1342cc8363be6f4aa0948b218a6427f1a22d76
Tyerkal <94529171+Tyerkal@users.noreply.github.com>
2023-07-29 19:17:22 +0530

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-let result = document.getElementById("result");
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
 
-function getResult(value) {
-	if (value == "=") {
-		result.value = eval(result.value);
-	} else {
-		result.value += value;
-	}
-}
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
 
-function clearResult() {
-	result.value = "";
-}
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        
+    })
+})
